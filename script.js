5e5378b7be97d9f428d4d314a258d128c43817a3
Samandar-space <yoldowevaferuza368@gmail.com>
2026-07-12 19:32:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+const todoInput = document.querySelector('todo-input');
+const addBtn = document.querySelector('add-btn');
+const todoList = document.querySelector('todo-list');
+
+
+addBtn.addEventListener('click', () => {
+    const taskText = todoInput.value.trim();
+
+    
+    if (taskText === '') {
+        alert('Iltimos, matn kiriting!');
+        return;
+    }
+
+    const li = document.createElement('li');
+    
+  
+    li.textContent = taskText;
+
+    const btnWrapper = document.createElement('span');
+    btnWrapper.innerHTML = `<button class="delete-btn">Delete</button>`;
+    li.append(btnWrapper);
+
+    const deleteBtn = btnWrapper.querySelector('.delete-btn');
+    deleteBtn.addEventListener('click', () => {
+        li.remove(); 
+    });
+
+    todoList.append(li);
+
+    todoInput.value = '';
+});
